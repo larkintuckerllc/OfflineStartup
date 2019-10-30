0b7ccca0be15ede39aaf4c1ed392c639fb69a91e
John Tucker <jtucker@appirio.com>
2019-10-30 19:57:03 -0400

    Done Maybe

--- src/components/App/AppLoading/index.tsx
+++ src/components/App/AppLoading/index.tsx
@@ -1,9 +1,38 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Text } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { getFirstLoad, setFirstLoad } from '../../../store/ducks/firstLoad';
+import AppHome from '../AppHome';
 
-// TODO: HANDLE FIRST
+const delay = (): Promise<void> =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, 5000);
+  });
+
 const AppLoading: FC = () => {
-  return <Text>AppLoading</Text>;
+  const dispatch = useDispatch();
+  const firstLoad = useSelector(getFirstLoad);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const execute = async (): Promise<void> => {
+      await delay();
+      dispatch(setFirstLoad(false));
+      setLoading(false);
+    };
+    execute();
+  }, [setLoading]);
+
+  if (loading) {
+    return (
+      <Text>
+        Loading&nbsp;
+        {firstLoad ? 'First' : 'Subsequent'}
+      </Text>
+    );
+  }
+  return <AppHome />;
 };
 
 export default AppLoading;
